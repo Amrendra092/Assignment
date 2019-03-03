2b286adeb3e00a85e209869a1502cb3f622007fa
Amrendra092 <amrendra.singh@tothenew.com>
2019-03-04 01:08:53 +0530

    java8

---
+++ java8/Question1.java
@@ -0,0 +1,34 @@
+interface Greater{
+    boolean greaterNum(int a,int b);
+}
+interface Increment{
+    int inc(int a);
+}
+interface ConcateStrings{
+    String conc(String a, String b);
+}
+interface Upper{
+    String upp(String a);
+}
+public class Question1 {
+    public static void main(String[] args) {
+
+        Greater greater = (int a, int b) -> {
+            if (a > b)
+                return true;
+            else
+                return false;
+        };
+        System.out.println( greater.greaterNum(5,6));
+
+        Increment increment=a-> ++a;
+        System.out.println(increment.inc(5));
+
+        ConcateStrings concateStrings=(a,b)->a.concat(b);
+        System.out.println(concateStrings.conc("amrendra","bahubali"));
+
+        Upper upper=a->a.toUpperCase();
+        System.out.println(upper.upp("you"));
+    }
+
+}
---
+++ java8/Question2.java
@@ -0,0 +1,9 @@
+interface Calculate<X>{
+    X sum(X a,X b);
+}
+public class Question2 {
+    public static void main(String[] args) {
+        Calculate<Integer> calculate=(a,b)->a+b;
+        System.out.println(calculate.sum(1,2));
+    }
+}
