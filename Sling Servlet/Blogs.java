6eb047b305e1ad231e9fe96b352c6a5fd7aa7b91
Amrendra Singh <amrendra.singh@tothenew.com>
2019-05-16 18:41:14 +0530

    sling servlet

---
+++ Sling Servlet/Blogs.java
@@ -0,0 +1,25 @@
+public class Blogs {
+
+        private String creation_date;
+        private String blog_name;
+
+        public Blogs() { }
+
+        public Blogs(String creation_date, String blog_name) {
+            this.creation_date=creation_date;
+            this.blog_name=blog_name;
+
+        }
+
+        public String getCreationDate() {
+            return creation_date;
+        }
+
+        @Override
+        public String toString() {
+            return "Blogs{" +
+                    "creation_date='" + creation_date + '\'' +
+                    ", blog_name='" + blog_name + '\'' +
+                    '}';
+        }
+    }
